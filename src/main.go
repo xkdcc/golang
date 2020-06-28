e70b037be1223f2563df546ef46a83a4844e998f
Brant Chen <2369047+xkdcc@user.noreply.github.com>
2020-06-27 17:27:19 -0700

    Some practices.

--- src/main.go
+++ src/main.go
@@ -2,11 +2,19 @@ package main
 
 import "fmt"
 
+type empolyee struct {
+	firstName, lastName string
+	salary              int
+}
+
 func main() {
 	r := "☞"
-	fmt.Printf("%x \n", r)
-	fmt.Printf("%v \n", r)
-	fmt.Printf("%T \n", r)
+	fmt.Printf("Print the lower casee base 16 by %%x : %x \n", r)
+	fmt.Printf("Print the default format by %%v      : %v \n", r)
+	fmt.Printf("Print type value by %%T              : %T \n", r)
 
 	fmt.Println()
+
+	var e empolyee
+	fmt.Println("Print Employee struct :\n", e)
 }
