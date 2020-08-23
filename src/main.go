46f80b0187e125a9a0b5cd3b56d0cd858fd7ff91
Brant <xkdcc@users.noreply.github.com>
2020-08-22 18:14:08 -0700

    Add json support (not all API works as expected, check details in comments)
    1. Get all posts works
    2. Get specify post works
    3. Delete specify post works
    4. Update/Create not work well

--- src/main.go
+++ src/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"math/rand"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -10,6 +13,70 @@ import (
 type empolyee struct {
 	firstName, lastName string
 	salary              int
+}
+
+type Post struct {
+	ID    string `json:"id"`
+	Title string `json:"title"`
+	Body  string `json:"body"`
+}
+
+var posts []Post
+
+func getPosts(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(posts)
+}
+
+func getPost(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range posts {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Post{})
+}
+
+func createPost(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var post Post
+	_ = json.NewDecoder(r.Body).Decode(post)
+	post.ID = strconv.Itoa(rand.Intn(1000000))
+	posts = append(posts, post)
+	json.NewEncoder(w).Encode(&post)
+}
+
+func updatePost(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range posts {
+		if item.ID == params["id"] {
+			posts = append(posts[:index], posts[index+1:]...)
+
+			var post Post
+			_ = json.NewDecoder(r.Body).Decode(post)
+			post.ID = params["id"]
+			posts = append(posts, post)
+			json.NewEncoder(w).Encode(&post)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(posts)
+}
+
+func deletePost(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range posts {
+		if item.ID == params["id"] {
+			posts = append(posts[:index], posts[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(posts)
 }
 
 func main() {
@@ -24,6 +91,15 @@ func main() {
 	fmt.Println("Print Employee struct :\n", e)
 
 	var m = mux.NewRouter()
+
+	posts = append(posts, Post{ID: "1", Title: "My first post", Body: "This is the content of my first post"})
+	posts = append(posts, Post{ID: "2", Title: "My second post", Body: "This is the content of my second post"})
+
+	m.HandleFunc("/posts", getPosts).Methods("GET")
+	m.HandleFunc("/posts", createPost).Methods("POST")
+	m.HandleFunc("/posts/{id}", getPost).Methods("GET")
+	m.HandleFunc("/posts/{id}", updatePost).Methods("PUT")
+	m.HandleFunc("/posts/{id}", deletePost).Methods("DELETE")
 
 	m.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
